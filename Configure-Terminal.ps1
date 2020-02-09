ed4ba88288ad8d79752d6e5e9c11c059c5ad4c0f
dark-coffee <9117837+dark-coffee@users.noreply.github.com>
2020-02-09 17:05:38 +0000

    Update Configure-Terminal.ps1

--- Configure-Terminal.ps1
+++ Configure-Terminal.ps1
@@ -5,7 +5,7 @@
 #Url: https://github.com/dark-coffee/Configure-Terminal
 
 #Pulls a custom manifest for my ps setup, downloads assoc scripts, imports.
-if (Test-Path "$pwd\down"){}else{ New-Item -Path "$PWD\down\" -ItemType Directory }
+if (!(Test-Path "$pwd\down")){ New-Item -Path "$PWD\down\" -ItemType Directory }
 
 $ManifestUrl =  "https://raw.githubusercontent.com/dark-coffee/Configure-Terminal/master/manifest.txt"
 $ManifestDest = "$PWD\down\manifest.txt"
